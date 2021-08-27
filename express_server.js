acc2ac0331c227005bce734a5dc9620d6541dc4a
Harjeet Kaur <harjeetkaur.dhanjal@gmail.com>
2021-08-27 14:15:05 -0400

    redirect url

--- express_server.js
+++ express_server.js
@@ -35,16 +35,22 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.post("/urls", (req, res) => {
+
+  let longUrl = req.body.longURL
+  let shortUrl = generateRandomString()
+  urlDatabase[shortUrl] = longUrl
+  res.redirect("/urls/" + shortUrl)
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  res.redirect(urlDatabase[req.params.shortURL])
+
+});
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: "http://www.lighthouselabs.ca" };
   res.render("urls_show", templateVars);
 });
-app.post("/urls", (req, res) => {
-  console.log(req.body);
-  console.log(generateRandomString()) // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
-});
-
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
